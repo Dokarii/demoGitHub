f63e5883066110579822cbd9c9afe41a0cba38e4
Dokarii <ricardotejedor18@gmail.com>
2024-10-31 08:21:17 -0500

    Se crea vehiculo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String name = "Richard Lee";
+        int age = 25;
+        boolean status = true;
     }
 }
